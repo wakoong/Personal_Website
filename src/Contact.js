e16375074d10a36393e314db9e36d94de864abbb
Woosik Koong <koongw@berkeley.edu>
2019-03-19 20:50:40 -0700

    contact page web ui

--- src/Contact.js
+++ src/Contact.js
@@ -3,8 +3,20 @@ import React from 'react';
 class Contact extends React.Component {
     render() {
         return(
-            <div className="contact">
-                Contact    
+            <div className="body-wrapper">
+                <div className="contact-form-wrapper">
+                    <form className="contact-form">
+                        <h1>Contact me</h1>
+                        <label>
+                            <input type="text" name="name" placeholder="Name" className="standard-input"/>
+                        </label>
+                        <label>
+                            <input type="text" name="email" placeholder="Email" className="standard-input" /> 
+                        </label>
+                        <textarea type="text" name="message" placeholder="Message" className="standard-textarea" />
+                        <input type="submit" value="SEND IT!" className="standard-submit-btn" />
+                    </form>
+                </div>
             </div>
         )
     }
