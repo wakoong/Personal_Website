acb5e4a00c2ae3f3e111651cba5ce00bab1057d4
Woosik Koong <woosik.koong@gmail.com>
2019-08-26 15:19:36 -0700

    pip virtualenv setup and pip installed robin stocks

--- backend/app.js
+++ backend/app.js
@@ -1,32 +1,6 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const app = express();
-const morgan = require('morgan');
-const { urlencoded, json } = require('body-parser');
+var express = require('express');
+var app = express();
 
-const BodyType = require('./db/models/workout/bodyType');
-const WorkoutType = require('./db/models/workout/workoutType'); 
-
-app.use(morgan('dev'))
-app.use(urlencoded({ extended: true }))
-app.use(json())
-
-const connect = () => {
-  return mongoose.connect('mongodb://localhost:27017/whatever')
-}
-
-connect() 
-  .then(async connection => {
-    console.log("app running on port:5000")
-    const workoutType = await WorkoutType.create({workoutType: 'Bench Press', sets: 3, reps: 10})
-    const bodyType = await BodyType.create({bodyType: 'Chest', workoutType: [workoutType._id]})
-
-    const match = await BodyType.findById(workoutType._id)
-      .populate('workoutType')
-      .exec()
-    console.log("match: ", match, workoutType, bodyType)
-    app.listen(5000)
-  })
-  .catch(e => console.error(e))
-
-
+app.listen(3001, function() {
+  console.log('server running at port 3001');
+});
---
+++ backend/script/robinhood.py
@@ -0,0 +1,1 @@
+import robin_stocks
