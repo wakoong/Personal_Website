4d2b0c813dd23bf976f22cece6505105d4378b31
Woosik Koong <woosik.koong@gmail.com>
2019-10-29 23:23:01 -0700

    Add packages (testing and react-window-size), setup babal, and update mongoose

--- .babelrc.js
+++ .babelrc.js
@@ -1,15 +1,13 @@
 const isTest = String(process.env.NODE_ENV) === 'test';
 
-// module.exports = {
-//   presets: [['env', {modules:{ isTest ? 'commonjs' : false }}], 'react'],
-//   plugins: [
-//     'syntax-dynamic-import',
-//     'transform-class-properties',
-//     'transform-object-rest-spread'
-//   ]
-// }
-
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
-  plugins: ['transform-class-properties', 'transform-object-rest-spread'],
+  presets: [
+    ['@babel/preset-env', { modules: isTest ? 'commonjs' : false }],
+    '@babel/preset-react',
+  ],
+  plugins: [
+    'syntax-dynamic-import',
+    'transform-class-properties',
+    'transform-object-rest-spread',
+  ],
 };
