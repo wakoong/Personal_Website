5fed9a1d6535dcfa4293f694b50be1d08416e930
Woosik Koong <woosik.koong@gmail.com>
2019-10-30 19:26:16 -0700

    Use Recharts to draw a simple bar chart
    1. Found out a library called Rechart which makes drawing graph on react environment much easier compared to using the D3 api from the scratch.
    
    2. I need to look into the tradeoffs of using Recharts over D3 but Recharts seems to be the better choice for me for now.

--- src/components/Laboratory/robinhood-stocks.tsx
+++ src/components/Laboratory/robinhood-stocks.tsx
@@ -1,12 +1,50 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts';
+
 class Stocks extends React.Component {
   render() {
     const { stocks, getTotal, getTable } = this.props;
     const stocksTotal = getTotal(stocks);
+
+    let data = stocks.map((s) => ({
+      name: s.symbol,
+      percentage: ((s.quantity * s.last_trade_price) / stocksTotal) * 100,
+    }));
+
     return (
       <div className='rb-page'>
+        <div className='rb-graph'>
+          <ResponsiveContainer width='90%' height='90%'>
+            <BarChart
+              data={data}
+              layout='vertical'
+              margin={{
+                top: 20,
+                right: 20,
+                left: 20,
+                bottom: 20,
+              }}>
+              <CartesianGrid strokeDasharray='3 3' />
+              <XAxis type='number' />
+              <YAxis dataKey='name' type='category' />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey='percentage' fill='#4682b4' />
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
         <div className='rb-table'>
           {getTable('Stocks', stocks, stocksTotal)}
         </div>
