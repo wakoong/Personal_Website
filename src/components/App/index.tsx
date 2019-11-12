50b24dcb388eae7df61e5c7e4279b4813d69deb0
Woosik Koong <woosik.koong@gmail.com>
2019-11-11 23:05:43 -0800

    Add preview page for laboratory cards and fixed prettier bug

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router } from '@reach/router';
-import { HashRouter } from 'react-router-dom';
-import { Provider as ReduxProvider } from 'react-redux';
+import {Router} from '@reach/router';
+import {HashRouter} from 'react-router-dom';
+import {Provider as ReduxProvider} from 'react-redux';
 
 import Header from '../Header/header.tsx';
 import Main from '../Main/main.tsx';
 import Laboratory from '../Laboratory/laboratory.tsx';
 import About from '../About/about.tsx';
 import Robinhood from '../Laboratory/robinhood.tsx';
+import CP from '../Laboratory/CP.tsx';
 import store from '../../store';
 
 class App extends React.Component {
@@ -22,6 +23,7 @@ class App extends React.Component {
             <About path='/about' />
             <Laboratory path='/laboratory' />
             <Robinhood path='/robinhood/*' />
+            <CP path='/coach-and-player' />
           </Router>
         </ReduxProvider>
       </div>
