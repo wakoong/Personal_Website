7ed97e234466af502749207e7c04ac1918994671
Woosik Koong <woosik.koong@gmail.com>
2020-09-29 02:56:15 -0700

    Refactor Header with styled-components - able to apply different themes through ThemeProvider

---
+++ src/components/Header.tsx
@@ -0,0 +1,88 @@
+import * as React from 'react';
+
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  background: ${(props) => props.theme.primaryColor};
+  color: ${(props) => props.theme.textColor};
+  text-transform: uppercase;
+
+  @media (min-width: 768px) {
+    flex-wrap: nowrap;
+  }
+`;
+
+const Logo = styled.h1`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  font-size: 2em;
+  font-weight: 900;
+  color: ${(props) => props.theme.textColor};
+  padding: 1em;
+  cursor: pointer;
+
+  &:hover {
+    text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000,
+      1px 1px 0 #000;
+  }
+
+  @media (min-width: 768px) {
+    width: auto;
+  }
+`;
+
+const Nav = styled.nav`
+  background: ${(props) => props.theme.primaryColor};
+  color: ${(props) => props.theme.textColor};
+  font-weight: 300;
+  margin: 0;
+  width: 100%;
+
+  ul {
+    display: flex;
+    justify-content: center;
+    margin: 0;
+    padding: 0;
+  }
+
+  li {
+    padding: 0 1em;
+    list-style: none;
+    cursor: pointer;
+
+    &:hover,
+    &:active {
+      text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000,
+        1px 1px 0 #000;
+    }
+  }
+
+  @media (min-width: 768px) {
+    width: auto;
+    ul {
+      justify-content: space-evenly;
+    }
+  }
+`;
+
+const Header = () => {
+  return (
+    <StyledHeader>
+      <Logo>WOOSIK.K</Logo>
+      <Nav>
+        <ul>
+          <li>Experience</li>
+          <li>Projects</li>
+          <li>Writings</li>
+        </ul>
+      </Nav>
+    </StyledHeader>
+  );
+};
+
+export default Header;
