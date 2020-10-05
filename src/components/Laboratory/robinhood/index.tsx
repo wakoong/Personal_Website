0e958b368de626686f5a4cbf73161a7526e2d908
Woosik Koong <woosik.koong@gmail.com>
2020-10-05 22:18:00 +0900

    Refactor css for robinhood project's landing page with styled-components

--- src/components/Laboratory/robinhood/index.tsx
+++ src/components/Laboratory/robinhood/index.tsx
@@ -1,77 +1,130 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React from 'react';
+import { connect } from 'react-redux';
+import styled from 'styled-components';
 
-import RAccount from './robinhood-account.tsx'
-import RobinhoodMain from './robinhood-main'
-import ETFs from './robinhood-etfs.tsx'
-import Stocks from './robinhood-stocks.tsx'
-import Button from '../Common/Material-UI/button.tsx'
-import logo from '../../../assets/images/rh-gorilla.png'
+import RAccount from './robinhood-account.tsx';
+import RobinhoodMain from './robinhood-main';
+import ETFs from './robinhood-etfs.tsx';
+import Stocks from './robinhood-stocks.tsx';
+import Button from '../Common/Material-UI/button.tsx';
+import logo from '../../../assets/images/rh-gorilla.png';
 
-import { loginWithData, getOverviewData } from './robinhood-account-redux.tsx'
+import { loginWithData, getOverviewData } from './robinhood-account-redux.tsx';
+
+const LandingStyles = styled.div`
+  position: relative;
+  height: calc(100vh - 6.5em);
+  width: 100%;
+  background-color: ${(props) => props.theme.primaryColor};
+  color: ${(props) => props.theme.textColor};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+
+  img {
+    position: absolute;
+    height: auto;
+    width: auto;
+  }
+
+  .loading {
+    text-align: center;
+    padding: 2em;
+    font-size: 1.5em;
+    font-weight: 900;
+  }
+
+  .disclaimer {
+    position: absolute;
+    bottom: 0;
+    padding: 5em;
+    max-width: 50em;
+    line-height: 1.4;
+  }
+`;
 
 class Robinhood extends React.Component {
-	_isMounted = false
-	componentDidMount() {
-		const { login } = this.props
-		this._isMounted = true
-		login()
-	}
+  _isMounted = false;
+  componentDidMount() {
+    const { login } = this.props;
+    this._isMounted = true;
+    login();
+  }
 
-	componentWillUnmount() {
-		this._isMounted = false
-	}
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
 
-	async componentDidUpdate(prevProps) {
-		if (prevProps.authenticated && prevProps.portfolio.length === 0 && prevProps.positions === '') {
-			try {
-				const { onOverviewData } = this.props
-				onOverviewData()
-			} catch (error) {
-				console.log(error)
-			}
-		}
-	}
+  async componentDidUpdate(prevProps) {
+    if (
+      prevProps.authenticated &&
+      prevProps.portfolio.length === 0 &&
+      prevProps.positions === ''
+    ) {
+      try {
+        const { onOverviewData } = this.props;
+        onOverviewData();
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
 
-	render() {
-		const { account, overview, portfolio, authenticated, orders } = this.props
+  render() {
+    const { account, overview, portfolio, authenticated, orders } = this.props;
 
-		let main
+    let main;
 
-		if (authenticated && overview.length !== 0) {
-			main = <RobinhoodMain account={account} portfolio={portfolio} overview={overview} orders={orders} />
-		} else {
-			main = (
-				<React.Fragment>
-					<div className="robinhood-login-wrapper">
-						<img src={logo} alt="rh project logo" />
-						<div>Loading...</div>
-					</div>
-					<div className="disclaimer">
-						A basic Heroku server stays inactive to save server resources when the application is unused.
-						Therefore, the application wil take longer time to load whenever the Heroku server needs to be
-						reactivated. Try refreshing the browser if you notice a delayed loading. Thank you.
-					</div>
-				</React.Fragment>
-			)
-		}
+    if (authenticated && overview.length !== 0) {
+      main = (
+        <RobinhoodMain
+          account={account}
+          portfolio={portfolio}
+          overview={overview}
+          orders={orders}
+        />
+      );
+    } else {
+      main = (
+        <React.Fragment>
+          <LandingStyles>
+            <img src={logo} alt='rh project logo' />
+            <div className='disclaimer'>
+              <div className='loading'>Loading...</div>
+              <p>
+                A basic Heroku server stays inactive to save server resources
+                when the application is unused. Therefore, the application wil
+                take longer time to load whenever the Heroku server needs to be
+                reactivated. Try refreshing the browser if you notice a delayed
+                loading. Thank you.
+              </p>
+            </div>
+          </LandingStyles>
+        </React.Fragment>
+      );
+    }
 
-		return <div className="project-background robinhood">{main}</div>
-	}
+    return <div className='project-background robinhood'>{main}</div>;
+  }
 }
 
 const mapStateToProps = (state) => ({
-	account: state.robinhoodAccount.account,
-	portfolio: state.robinhoodAccount.portfolio,
-	authenticated: state.robinhoodAccount.logged_in,
-	orders: state.robinhoodAccount.orders,
-	positions: state.robinhoodAccount.positions,
-	overview: state.robinhoodAccount.overview
-})
+  account: state.robinhoodAccount.account,
+  portfolio: state.robinhoodAccount.portfolio,
+  authenticated: state.robinhoodAccount.logged_in,
+  orders: state.robinhoodAccount.orders,
+  positions: state.robinhoodAccount.positions,
+  overview: state.robinhoodAccount.overview,
+});
 
 const mapDispatchToProps = (dispatch) => ({
-	login: () => dispatch(loginWithData()),
-	onOverviewData: () => dispatch(getOverviewData())
-})
+  login: () => dispatch(loginWithData()),
+  onOverviewData: () => dispatch(getOverviewData()),
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(Robinhood)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Robinhood);
