a4dee67180c9d78555854f5060ab41699f309f41
Woosik Koong <woosik.koong@gmail.com>
2020-10-15 15:26:00 +0900

    Add types for themes

--- src/utils/themes.ts
+++ src/utils/themes.ts
@@ -9,14 +9,17 @@ import {
   yellowO,
 } from './colors';
 
-// enum Themes {
-//   DEFAULT,
-//   OAKLAND,
-//   WARRIORS,
-//   GIANTS,
-// }
+export interface IThemeProps {
+  emphasisColor: string;
+  hoverBackgroundColor: string;
+  inputBackgroundColor: string;
+  lightColor: string;
+  primaryColor: string;
+  selectColor: string;
+  textColor: string;
+}
 
-export const DefaultTheme = {
+export const DefaultTheme: IThemeProps = {
   emphasisColor: base[900],
   hoverBackgroundColor: base[300],
   inputBackgroundColor: base[400],
@@ -24,14 +27,9 @@ export const DefaultTheme = {
   primaryColor: base[50],
   selectColor: base[500],
   textColor: base[900],
-  /**
-   * grey #7f7f7f
-   * black #000000
-   * white #f7f7f7
-   */
 };
 
-export const GoldenStateTheme = {
+export const GoldenStateTheme: IThemeProps = {
   emphasisColor: yellowG[500],
   hoverBackgroundColor: yellowG[400],
   inputBackgroundColor: base[400],
@@ -41,7 +39,7 @@ export const GoldenStateTheme = {
   textColor: yellowG[100],
 };
 
-export const OaklandAsTheme = {
+export const OaklandAsTheme: IThemeProps = {
   emphasisColor: greenO[500],
   hoverBackgroundColor: greenO[200],
   inputBackgroundColor: base[400],
@@ -51,7 +49,7 @@ export const OaklandAsTheme = {
   textColor: blackS[500],
 };
 
-export const SfGiantTheme = {
+export const SfGiantTheme: IThemeProps = {
   emphasisColor: whiteS[500],
   hoverBackgroundColor: orangeS[400],
   inputBackgroundColor: base[400],
@@ -61,7 +59,16 @@ export const SfGiantTheme = {
   textColor: whiteS[500],
 };
 
-export const themes = [
+interface IThemes {
+  name:
+    | 'Default'
+    | 'Golden State Warriors'
+    | 'Oakland Athletics'
+    | 'San Francisco Giants';
+  theme: IThemeProps;
+}
+
+export const themes: IThemes[] = [
   { name: 'Default', theme: DefaultTheme },
   {
     name: 'Golden State Warriors',
