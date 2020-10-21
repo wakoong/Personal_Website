f133ea2d713934e576bdce17a8c4f0400c52f6ce
Woosik Koong <woosik.koong@gmail.com>
2020-10-20 19:57:09 -0700

    Add Button style components - need more flexibility

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
+interface IButtonStyleProps {
+  background: string | null | undefined;
+  padding: string,
+}
+
 const Button = styled.button`
-  color: ${props => props.color ? props.color : null};
-  padding: 1em 2em;
+  background: ${(props: IButtonStyleProps) => props.background ? props.background: null};
+  color: ${(props) => props.color ? props.color : null};
+  padding: ${(props) => props.padding ? props.padding : '1em 2em'};
   cursor: pointer;
 `
 
@@ -11,10 +17,11 @@ interface IButtonProps {
   background?: string;
   className?: string;
   color?: string;
+  padding?: string;
   text: string;
-  onClick?: boolean | (() => void);
+  onClick?: () => void;
 }
 
-export default ({ className, color, onClick, text}: IButtonProps) => {
-  return <Button className={className} color={color} onClick={onClick}>{text}</Button>
+export default ({ className, background, color, onClick, padding, text}: IButtonProps) => {
+  return <Button className={className} background={background} color={color} padding={padding} onClick={onClick}>{text}</Button>
 }
